1fb26cd7e32038b7beea245156400846151c6863
Arnish-val <rajarnish28@gmail.com>
2026-06-02 22:48:49 +0530

    added insert feature

--- index.js
+++ index.js
@@ -22,19 +22,40 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// GET home page
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+
 app.get("/", async (req, res) => {
-  try {
-    const result = await db.query("SELECT country_code FROM visited_countries");
-    let countries = [];
-    result.rows.forEach((country) => {
-      countries.push(country.country_code);
-    });
-    console.log(result.rows);
-    res.render("index.ejs", { countries: countries, total: countries.length });
-  } catch (err) {
-    console.error("Error executing query:", err.stack);
-    res.status(500).send("Database connection error. Make sure your local Database is running.");
+  const countries = await checkVisisted();
+  res.render("index.ejs", { countries: countries, total: countries.length });
+});
+
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
   }
 });
 
