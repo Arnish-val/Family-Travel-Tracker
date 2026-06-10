20ef5396cbb81655ef7a7bc0b16900e985ec51b2
Arnish-val <rajarnish28@gmail.com>
2026-06-10 22:45:47 +0530

    feat(backend): retrieve visited countries in chronological order

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ let users = [
 
 async function checkVisisted() {
   const result = await db.query(
-    "SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1; ",
+    "SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1 ORDER BY visited_countries.id ASC; ",
     [currentUserId]
   );
   let countries = [];
